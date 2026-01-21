47f875725a3ae3201b64a3b4d9987ffef1b59dec
Tanmay Vishwakarma <tanmay06lko@gmail.com>
2026-01-22 00:05:22 +0530

    add better-auth setup

---
+++ components/dashboard-ui.tsx
@@ -0,0 +1,8 @@
+
+export default function DashboardUI() {
+    return (
+        <div className="bg-teal-600">
+            DASHBOARD UI
+        </div>
+    )
+}
---
+++ lib/auth.ts
@@ -0,0 +1,10 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "../lib/generated/prisma/client";
+import prisma from "./prisma";
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql", // or "mysql", "postgresql", ...etc
+    }),
+});
